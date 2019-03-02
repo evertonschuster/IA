76ae7b51f8ec19c8611015396be1ae45e4fe01b7
everton <everton.schuster98@gmail.com>
2019-03-02 17:22:28 -0300

    create

---
+++ CarteiroFormiga/CarteiroFormiga/CarteiroFormiga/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarteiroFormiga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
